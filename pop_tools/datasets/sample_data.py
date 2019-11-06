c8b8d2511bc56cb86750295c6f5ad13b220e9cea
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-11-06 12:50:24 -0700

    Remove irrelevant datasets from the registry

--- pop_tools/datasets/sample_data.py
+++ pop_tools/datasets/sample_data.py
@@ -7,9 +7,6 @@ import os
 import aletheia_data
 
 URLS = {
-    'daily_reference_height_temperature.nc': 'ftp://ftp.cgd.ucar.edu/archive/aletheia-data/cesm-data/atm/daily_reference_height_temperature.nc',
-    'monthly_temperature.nc': 'ftp://ftp.cgd.ucar.edu/archive/aletheia-data/cesm-data/atm/monthly_temperature.nc',
-    'hourly6_total_precipitation_rate.nc': 'ftp://ftp.cgd.ucar.edu/archive/aletheia-data/cesm-data/atm/hourly6_total_precipitation_rate.nc',
     'tend_zint_100m_Fe.nc': 'ftp://ftp.cgd.ucar.edu/archive/aletheia-data/cesm-data/ocn/tend_zint_100m_Fe.nc',
     'iron_tracer.nc': 'ftp://ftp.cgd.ucar.edu/archive/aletheia-data/cesm-data/ocn/iron_tracer.nc',
     'daily_surface_potential_temperature.nc': 'ftp://ftp.cgd.ucar.edu/archive/aletheia-data/cesm-data/ocn/daily_surface_potential_temperature.nc',
