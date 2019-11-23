0deb242d771bf6ce85436823e1854166232459cb
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-11-23 14:29:20 -0700

    Test against Python3.8

--- ci/install-circle.sh
+++ ci/install-circle.sh
@@ -3,12 +3,10 @@
 set -e
 set -eo pipefail
 
-apt-get update && apt-get install subversion git -y
-
 conda config --set always_yes true --set changeps1 false --set quiet true
 conda config --add channels conda-forge
-conda env create -f ci/environment-dev-${PYTHON}.yml --name=${ENV_NAME} --quiet
-conda env list
+conda create --name=${ENV_NAME} python=${PYTHON} --quiet
+conda env update -f ci/environment.yml
 source activate ${ENV_NAME}
-pip install --no-deps --quiet -e .
+python -m pip install --no-deps --quiet -e .
 conda list -n ${ENV_NAME}
