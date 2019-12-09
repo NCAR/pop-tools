21cca8090ff5faec320da6e257adaaa6b936a570
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-12-09 11:19:12 -0700

    Add test for UEU, VNU variables

--- tests/test_xgcm_util.py
+++ tests/test_xgcm_util.py
@@ -1,3 +1,4 @@
+import pytest
 import xarray as xr
 import xgcm
 
@@ -5,9 +6,10 @@ import pop_tools
 from pop_tools import DATASETS
 
 
-def test_xgcm_grid():
-    fname = DATASETS.fetch('tend_zint_100m_Fe.nc')
-    ds = xr.open_dataset(fname)
+@pytest.mark.parametrize('file', ['tend_zint_100m_Fe.nc', 'g.e20.G.TL319_t13.control.001_hfreq.nc'])
+def test_xgcm_grid(file):
+    filepath = DATASETS.fetch(file)
+    ds = xr.open_dataset(filepath)
     grid = pop_tools.get_xgcm_grid(ds, metrics=None)
     assert isinstance(grid, xgcm.Grid)
     assert set(['X', 'Y', 'Z']) == set(grid.axes.keys())
