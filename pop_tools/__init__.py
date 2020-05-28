9e3b1d4ecf333c507ea561ae8524b3c6232bea2e
Riley Brady <riley.brady@colorado.edu>
2020-05-28 13:13:08 -0600

    remove old reference to config module

--- pop_tools/__init__.py
+++ pop_tools/__init__.py
@@ -5,7 +5,6 @@ from pkg_resources import DistributionNotFound, get_distribution
 
 from .datasets import DATASETS
 from .budget import regional_tracer_budget
-from .config import grid_defs
 from .eos import compute_pressure, eos
 from .fill import lateral_fill, lateral_fill_np_array
 from .grid import get_grid, grid_defs
