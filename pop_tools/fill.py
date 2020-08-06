ab2dbe885b0dbd31d619a1548885e0f615e1793d
fobryan3 <bryan@ucar.edu>
2020-08-06 11:20:05 -0600

    t formatting fixer e Please enter the commit message for your changes. Lines starting

--- pop_tools/fill.py
+++ pop_tools/fill.py
@@ -3,8 +3,9 @@ import xarray as xr
 from numba import jit
 
 
-def lateral_fill(da_in, isvalid_mask, ltripole=False, tol=1.0e-4,
-                 use_sor=False, rc=1.8, max_iter=1000):
+def lateral_fill(
+    da_in, isvalid_mask, ltripole=False, tol=1.0e-4, use_sor=False, rc=1.8, max_iter=1000
+):
     """Perform lateral fill on xarray.DataArray
 
     Parameters
@@ -59,15 +60,17 @@ def lateral_fill(da_in, isvalid_mask, ltripole=False, tol=1.0e-4,
         isvalid_mask_stack = isvalid_mask.stack(non_lateral_dims=non_lateral_dims)
         for i in range(da_in_stack.shape[-1]):
             arr = da_in_stack.data[:, :, i]
-            da_out_stack[:, :, i] = lateral_fill_np_array(arr, isvalid_mask_stack.data[:, :, i],
-                                                          ltripole,tol,use_sor,rc,max_iter)
+            da_out_stack[:, :, i] = lateral_fill_np_array(
+                arr, isvalid_mask_stack.data[:, :, i], ltripole, tol, use_sor, rc, max_iter
+            )
 
         da_out = da_out_stack.unstack('non_lateral_dims').transpose(*dims_in)
 
     else:
         da_out = xr.full_like(da_in, fill_value=np.nan)
-        da_out[:, :] = lateral_fill_np_array(da_in.data, isvalid_mask.data,
-                                             ltripole,tol,use_sor,rc,max_iter)
+        da_out[:, :] = lateral_fill_np_array(
+            da_in.data, isvalid_mask.data, ltripole, tol, use_sor, rc, max_iter
+        )
 
     da_out.attrs = attrs
     da_out.encoding = encoding
@@ -77,8 +80,9 @@ def lateral_fill(da_in, isvalid_mask, ltripole=False, tol=1.0e-4,
     return da_out
 
 
-def lateral_fill_np_array(var, isvalid_mask, ltripole=False, tol=1.0e-4,
-                          use_sor=False, rc=1.8, max_iter=1000):
+def lateral_fill_np_array(
+    var, isvalid_mask, ltripole=False, tol=1.0e-4, use_sor=False, rc=1.8, max_iter=1000
+):
 
     """Perform lateral fill on numpy.array
 
@@ -126,12 +130,12 @@ def lateral_fill_np_array(var, isvalid_mask, ltripole=False, tol=1.0e-4,
 
     var = var.copy()
 
-    if use_sor :
+    if use_sor:
         fillmask = np.isnan(var)  # Fill all NaNs
         keepNaNs = ~isvalid_mask & np.isnan(var)
         _iterative_fill_sor(nlat, nlon, var, fillmask, tol, rc, max_iter, ltripole)
-        var[keepNaNs] = np.nan    # Replace NaNs in areas not designated for filling
-    else :
+        var[keepNaNs] = np.nan  # Replace NaNs in areas not designated for filling
+    else:
         fillmask = np.isnan(var) & isvalid_mask
         missing_value = 1e36
         var[np.isnan(var)] = missing_value
@@ -220,85 +224,92 @@ def _iterative_fill_POP_core(nlat, nlon, var, fillmask, missing_value, tol, ltri
 
         var[1:nlat, :] = work[1:nlat, :]
 
+
 @jit(nopython=True)
-def _iterative_fill_sor(nlat, nlon, var, fillmask, tol=5.0e-4,
-            rc=1.6, max_iter=100, ltripole=False):
+def _iterative_fill_sor(
+    nlat, nlon, var, fillmask, tol=5.0e-4, rc=1.6, max_iter=100, ltripole=False
+):
     """Iterative land fill algorithm via SOR solution of Laplace Equation."""
 
     # Compute a zonal mean to use as a first guess
     # Apprarently jit doesn't like masked arrays so loop it out
     zoncnt = np.zeros(nlat)
     zonavg = np.zeros(nlat)
-    for j in range(0,nlat) :
-        zoncnt[j] = np.sum(np.where(fillmask[j,:],0,1))
-        zonavg[j] = np.sum(np.where(fillmask[j,:],0,var[j,:]))
-        if zoncnt[j] != 0 : zonavg[j] = zonavg[j]/zoncnt[j]
+    for j in range(0, nlat):
+        zoncnt[j] = np.sum(np.where(fillmask[j, :], 0, 1))
+        zonavg[j] = np.sum(np.where(fillmask[j, :], 0, var[j, :]))
+        if zoncnt[j] != 0:
+            zonavg[j] = zonavg[j] / zoncnt[j]
 
     # Fill missing zonal averages for rows that are entirely land
-    for j in range(0,nlat-1) :   # northward pass
-        if zoncnt[j] > 0 and zoncnt[j+1] == 0:
-            zoncnt[j+1]=1
-            zonavg[j+1] = zonavg[j]
-    for j in range(0,nlat-1) :  # southward pass
-        jrev = nlat-1-j
-        if zoncnt[jrev] > 0 and zoncnt[jrev-1] == 0 : 
-            zoncnt[jrev-1]=1
-            zonavg[jrev-1] = zonavg[jrev]
+    for j in range(0, nlat - 1):  # northward pass
+        if zoncnt[j] > 0 and zoncnt[j + 1] == 0:
+            zoncnt[j + 1] = 1
+            zonavg[j + 1] = zonavg[j]
+    for j in range(0, nlat - 1):  # southward pass
+        jrev = nlat - 1 - j
+        if zoncnt[jrev] > 0 and zoncnt[jrev - 1] == 0:
+            zoncnt[jrev - 1] = 1
+            zonavg[jrev - 1] = zonavg[jrev]
 
     # Replace the input array missing values with zonal average as first guess
-    for j in range(0,nlat) :
-        for i in range(0,nlon) :
-            if fillmask[j,i] : var[j,i] = zonavg[j]
+    for j in range(0, nlat):
+        for i in range(0, nlon):
+            if fillmask[j, i]:
+                var[j, i] = zonavg[j]
 
     # Now do the iterative 2D fill
-    res = np.zeros((nlat,nlon))  # work array hold residuals
+    res = np.zeros((nlat, nlon))  # work array hold residuals
     res_max = tol
     iter = 0
     while iter < max_iter and res_max >= tol:
-        res = res*0.0  # reset the residual to zero for this iteration
+        res = res * 0.0  # reset the residual to zero for this iteration
 
         # assume bottom row is all land, leave it set to zonal average
         # deal with top row separately below
-        for j in range(1, nlat-1):
+        for j in range(1, nlat - 1):
             jm1 = j - 1
             jp1 = j + 1
 
             for i in range(0, nlon):
                 if fillmask[j, i]:
                     im1 = i - 1
-                    if i == 0:                  # assume periodic in x
+                    if i == 0:  # assume periodic in x
                         im1 = nlon - 1
                     ip1 = i + 1
                     if i == nlon - 1:
                         ip1 = 0
 
                     # this is SOR
-                    res[j,i] = var[j,ip1] + var[j,im1] + var[jm1,i] + var[jp1,i] - 4.0*var[j,i]
-                    var[j,i] = var[j,i] + rc*0.25*res[j,i]
+                    res[j, i] = (
+                        var[j, ip1] + var[j, im1] + var[jm1, i] + var[jp1, i] - 4.0 * var[j, i]
+                    )
+                    var[j, i] = var[j, i] + rc * 0.25 * res[j, i]
 
         # do the top row if there was some valid data there in the input
         # otherwise leave it set to zonal average of northernmost row with valid data
-        if  zoncnt[nlat-1] > 1 :
-            j = nlat-1
-            jm1 = j-1
+        if zoncnt[nlat - 1] > 1:
+            j = nlat - 1
+            jm1 = j - 1
             jp1 = j
-            for i in range(0,nlon) :
-                if fillmask[j,i] :
-                    im1 = i-1
+            for i in range(0, nlon):
+                if fillmask[j, i]:
+                    im1 = i - 1
                     if i == 0:
                         im1 = nlon - 1
-                    ip1 = i+1
+                    ip1 = i + 1
                     if i == nlon - 1:
                         ip1 = 0
-                    io = nlon-1-i
+                    io = nlon - 1 - i
 
-                    if ltripole :  # use cross-pole periodicity
-                        res[j,i] = var[j,ip1] + var[j,im1] + var[jp1,io] + var[jm1,i] - 4.0*var[j,i]
-                        var[j,i] = var[j,i] + rc*0.25*res[j,i]
-                    else:          # do a 1D smooth on pole row
-                        res[j,i] = var[j,ip1] + var[j,im1] - 2.0*var[j,i]
-                        var[j,i] = var[j,i] + rc*0.5*res[j,i]
+                    if ltripole:  # use cross-pole periodicity
+                        res[j, i] = (
+                            var[j, ip1] + var[j, im1] + var[jp1, io] + var[jm1, i] - 4.0 * var[j, i]
+                        )
+                        var[j, i] = var[j, i] + rc * 0.25 * res[j, i]
+                    else:  # do a 1D smooth on pole row
+                        res[j, i] = var[j, ip1] + var[j, im1] - 2.0 * var[j, i]
+                        var[j, i] = var[j, i] + rc * 0.5 * res[j, i]
 
-
-        res_max = np.max(np.fabs(res))/np.max(np.fabs(var))
+        res_max = np.max(np.fabs(res)) / np.max(np.fabs(var))
         iter += 1
