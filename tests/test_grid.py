ca3c67c69473ba336e59db5931415bbb92b93fcf
Kevin Paul <kpaul@ucar.edu>
2020-09-08 17:01:46 -0600

    Fix get_grid when region_mask_regions data is present (#64)
    * Remove region_mask_regions from get_grid attrs
    
    Also add a test to ensure get_grid().to_netcdf() works and fixed a typo in a
    function name in region_masks. Still need to add region_mask_regions back as a
    DataArray.
    
    * First pass at writing region names/values arrays
    
    * Ignore VSCode files
    
    * Adding region name/val arrays to dataset
    
    * Use NetCDF format when writing file
    
    Co-authored-by: Anderson Banihirwe <axbanihirwe@ualr.edu>
    
    * Pop from dso.attrs not grid_attrs
    
    * Add test for 2x get_grid call
    
    Co-authored-by: Michael Levy <mike.levy.work@gmail.com>
    Co-authored-by: Anderson Banihirwe <axbanihirwe@ualr.edu>

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -26,3 +26,20 @@ def test_get_grid_scrip():
     ds_test = pop_tools.get_grid('POP_gx3v7', scrip=True)
     ds_ref = xr.open_dataset(DATASETS.fetch('POP_gx3v7.nc'))
     assert ds_compare(ds_test, ds_ref, assertion='allclose', rtol=1e-14, atol=1e-14)
+
+
+def test_get_grid_twice():
+    ds1 = pop_tools.get_grid('POP_gx1v7')
+    ds2 = pop_tools.get_grid('POP_gx1v7')
+    xr.testing.assert_identical(ds1, ds2)
+
+
+def test_get_grid_to_netcdf():
+    for grid in pop_tools.grid_defs.keys():
+        print('-' * 80)
+        print(grid)
+        ds = pop_tools.get_grid(grid)
+        for format in ['NETCDF4', 'NETCDF3_64BIT']:
+            gridfile = f'{grid}_{format}.nc'
+            ds.to_netcdf(gridfile, format=format)
+            os.system(f'rm -f {gridfile}')
