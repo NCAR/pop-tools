cc4a82e30c5b544bf73b21faa979a9712aac5048
matt-long <mclong@ucar.edu>
2021-04-17 10:53:24 -0600

    linting

--- pop_tools/__init__.py
+++ pop_tools/__init__.py
@@ -3,9 +3,9 @@
 
 from pkg_resources import DistributionNotFound, get_distribution
 
+from .calc import cfc11sol, cfc12sol
 from .datasets import DATASETS
 from .eos import compute_pressure, eos
-from .calc import cfc12sol, cfc11sol
 from .fill import lateral_fill, lateral_fill_np_array
 from .grid import get_grid, grid_defs
 from .region_masks import list_region_masks, region_mask_3d
