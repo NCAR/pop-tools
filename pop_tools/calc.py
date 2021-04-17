061b343fcfb87d2f9df01e71fefe6305a6001518
matt-long <mclong@ucar.edu>
2021-04-17 10:50:27 -0600

    pre-commit compliance

--- pop_tools/calc.py
+++ pop_tools/calc.py
@@ -4,22 +4,22 @@ import numpy as np
 def cfc12sol(SALT, TEMP):
     """
     Compute CFC12 solubility in seawater.
-    Reference: 
-      Warner & Weiss (1985) , Deep Sea Research, vol32 
+    Reference:
+      Warner & Weiss (1985) , Deep Sea Research, vol32
       doi:10.1016/0198-0149(85)90099-8
-    
+
     Parameters
     ----------
-    
+
     SALT : float
       salinity, psu
     TEMP : float
        potential temperature, degree C
-    
+
     Returns
     -------
-    
-    SOLUBILITY_CFC12: returned value in mol/m3/patm    
+
+    SOLUBILITY_CFC12: returned value in mol/m3/patm
     """
     return _calc_cfcsol(SALT, TEMP, 12)
 
@@ -27,31 +27,31 @@ def cfc12sol(SALT, TEMP):
 def cfc11sol(SALT, TEMP):
     """
     Compute CFC11 solubility in seawater.
-    Reference: 
-      Warner & Weiss (1985) , Deep Sea Research, vol32 
+    Reference:
+      Warner & Weiss (1985) , Deep Sea Research, vol32
       doi:10.1016/0198-0149(85)90099-8
-    
+
     Parameters
     ----------
-    
+
     SALT : float
       salinity, psu
     TEMP : float
        potential temperature, degree C
-    
+
     Returns
     -------
-    
-    SOLUBILITY_CFC11: returned value in mol/m3/patm    
-    """    
+
+    SOLUBILITY_CFC11: returned value in mol/m3/patm
+    """
     return _calc_cfcsol(SALT, TEMP, 11)
 
 
-def _calc_cfcsol(PS, PT, kn):  
+def _calc_cfcsol(PS, PT, kn):
     """
     Compute CFC11 and CFC12 Solubilities in seawater.
         Reference: Warner & Weiss (1985) , Deep Sea Research, vol32
-    
+
     Translated from cfc11_mod.F90 (MCL, 2011)
     INPUT:
     PT: temperature (degree Celsius)
@@ -61,8 +61,8 @@ def _calc_cfcsol(PS, PT, kn):
     SOLUBILITY_CFC: returned value in mol/m3/pptv
     1 pptv = 1 part per trillion = 10^-12 atm = 1 picoatm
     """
-    T0_Kelvin = 273.16 # this is indeed what POP uses
-      
+    T0_Kelvin = 273.16  # this is indeed what POP uses
+
     assert kn in [11, 12], 'kn must be either 11 or 12'
     if kn == 11:
         a1 = -229.9261
@@ -80,16 +80,16 @@ def _calc_cfcsol(PS, PT, kn):
         b1 = -0.143566
         b2 = 0.091015
         b3 = -0.0153924
-    
-    WORK = ((PT + T0_Kelvin) * 0.01)
+
+    WORK = (PT + T0_Kelvin) * 0.01
 
     #  coefficient for solubility in  mol/l/atm
-    SOLUBILITY_CFC = np.exp( a1 + a2 / WORK + a3 * np.log ( WORK )
-                           + a4 * WORK * WORK
-                           + PS * ( ( b3 * WORK + b2 ) * WORK + b1 ) )
+    SOLUBILITY_CFC = np.exp(
+        a1 + a2 / WORK + a3 * np.log(WORK) + a4 * WORK * WORK + PS * ((b3 * WORK + b2) * WORK + b1)
+    )
 
     #  conversion from mol/(l * atm) to mol/(m^3 * atm) to mol/(m3 * pptv)
-    SOLUBILITY_CFC = 1000. * SOLUBILITY_CFC
+    SOLUBILITY_CFC = 1000.0 * SOLUBILITY_CFC
     SOLUBILITY_CFC = 1.0e-12 * SOLUBILITY_CFC
 
-    return SOLUBILITY_CFC # mol/m^3/patm
+    return SOLUBILITY_CFC  # mol/m^3/patm
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,12 +1,13 @@
 import numpy as np
+
 import pop_tools
 
 
 def test_cfc12_numpy_1():
-    sol = pop_tools.cfc12sol(35., 15.)
+    sol = pop_tools.cfc12sol(35.0, 15.0)
     np.testing.assert_almost_equal(sol, 3.103297535228039e-12, decimal=14)
 
-    
+
 def test_cfc11_numpy_1():
-    sol = pop_tools.cfc11sol(35., 15.)
-    np.testing.assert_almost_equal(sol, 1.149632255646373e-11, decimal=14)    
+    sol = pop_tools.cfc11sol(35.0, 15.0)
+    np.testing.assert_almost_equal(sol, 1.149632255646373e-11, decimal=14)
