85321728ab2e136318ca736c70a8bc18fbe0012f
Stephen G Yeager <yeager@ucar.edu>
2023-03-03 16:54:10 -0700

    Mods to eos.py to improve dask compatibility (#149)
    * eos() mods to improve dask functionality
    
    * correcting tab errors
    
    * Support scalar depth
    
    ---------
    
    Co-authored-by: dcherian <deepak@cherian.net>

--- pop_tools/eos.py
+++ pop_tools/eos.py
@@ -75,17 +75,11 @@ def eos(salt, temp, return_coefs=False, **kwargs):
 
     use_xarray = False
     if any(isinstance(arg, xr.DataArray) for arg in [salt, temp, d_or_p]):
-        if not all(isinstance(arg, xr.DataArray) for arg in [salt, temp, d_or_p]):
-            raise ValueError('cannot operate on mixed types')
         use_xarray = True
 
     # compute pressure
     if pressure is None:
-        if use_xarray:
-            pressure = xr.full_like(depth, fill_value=np.nan)
-            pressure[:] = 10.0 * compute_pressure(depth.data)  # dbar
-        else:
-            pressure = 10.0 * compute_pressure(depth)  # dbar
+        pressure = 10.0 * compute_pressure(depth)  # dbar
 
     # enforce min/max values
     tmin = -2.0
@@ -96,10 +90,7 @@ def eos(salt, temp, return_coefs=False, **kwargs):
     if use_xarray:
         temp = temp.clip(tmin, tmax)
         salt = salt.clip(smin, smax)
-
-        salt, temp, pressure = xr.broadcast(salt, temp, pressure)
-        if isinstance(salt.data, dask.array.Array):
-            pressure = pressure.chunk(salt.chunks)
+        salt, temp = xr.broadcast(salt, temp)
 
         if return_coefs:
             RHO, dRHOdS, dRHOdT = _compute_eos_coeffs(salt, temp, pressure)
